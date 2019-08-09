e4e5b2c1577ef873b2baebb251b2e5eb359dab5d
Austinlp4 <apendergrast44@gmail.com>
2019-08-09 12:37:42 -0500

    add to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import socket from 'socket.io-client';
 
-function App() {
+class App extends React.Component {
+
+  videocontainer = React.createRef();
+
+  componentDidMount(){
+    // let openSocket = socket('https://zyro-one.herokuapp.com/');
+    const config = window.config;
+    console.log(config);
+
+  }
+
+  onRemoteStream = (element) => {
+    this.videocontainer.appendChild(element);
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="experiment">
+        <section>
+          <select id="broadcasting-option">
+            <option value="Audio + Video">Audio + Video</option>
+            <option value="Only Audio">Only Audio</option>
+            <option value="Screen">Screen</option>
+          </select>
+          <input type="text" id="broadcast-name"/>
+          <button id="setup-new-broadcast" className="setup">Setup New Broadcast</button>
+
+          <table id="rooms-list"></table>
+          <div 
+            id="videos-container"
+            ref={this.videocontainer}
+            ></div>
+        </section>
+      </section>
     </div>
   );
+}
 }
 
 export default App;
