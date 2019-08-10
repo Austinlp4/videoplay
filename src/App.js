3503875eaac9a55fdd68878fa37cf1d039574ced
Austinlp4 <apendergrast44@gmail.com>
2019-08-10 11:46:47 -0500

    screen share and webcam broadcast

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import { broadcast } from './javascript/broadcast';
 class App extends React.Component {
 
   state = {
-    broadcastName: ''
+    broadcastName: '',
+    option: 'Audio + Video',
+    video: null,
+    stream: null
   }
 
   videocontainer = React.createRef();
   broadcastname = React.createRef();
   setupnewbroadcast = React.createRef();
+  broadcastervideo = React.createRef();
   config = window.config;
   DetectRTC = window.DetectRTC;
   broadcastUI = broadcast(this.config);
 
   componentDidMount(){
     // let openSocket = socket('https://zyro-one.herokuapp.com/');
-
+    console.log(window)
+    
   }
 
   handleChange = event => {
@@ -50,16 +55,13 @@ class App extends React.Component {
   }
 
   setupNewBroadcastButtonClickHandler = () => {
-    this.broadcastname.disabled = true;
-    this.setupnewbroadcast.disabled = true;
-
     this.DetectRTC.load(() => {
-      captureUserMedia(() => {
+      this.captureUserMedia(() => {
         let shared = 'video';
-        if(window.option == 'Only Audio') {
+        if(window.option === 'Only Audio') {
           shared = 'audio';
         }
-        if(window.option == 'screen'){
+        if(window.option === 'screen'){
           shared = 'screen'
         }
 
@@ -73,10 +75,93 @@ class App extends React.Component {
 
         this.broadcastUI.createRoom({
           roomName: roomName,
-          isAudio: shared === 'audio';
+          isAudio: shared === 'audio'
         })
       })
     })
+  }
+
+  captureUserMedia = (callback) => {
+    let constraints = null;
+    window.option = this.state.option;
+    let option = window.option;
+
+    if (option === 'Only Audio') {
+      constraints = {
+          audio: true,
+          video: false
+      };
+      if(this.DetectRTC.hasMicrophone !== true) {
+          alert('DetectRTC library is unable to find microphone; maybe you denied microphone access once and it is still denied or maybe microphone device is not attached to your system or another app is using same microphone.');
+      }
+      }
+      if (option === 'Screen') {
+          constraints = {
+              audio: false,
+              video: {
+                mandatory: {
+                  chromeMediaSource: 'desktop',
+                  maxWidth: 1920,
+                  maxHeight: 1080,
+                  maxFrameRate: 10,
+                  minAspectRatio: 1.77,
+                  // chromeMediaSourceId: sourceId         
+                }
+            }
+          };
+          if(this.DetectRTC.isScreenCapturingSupported !== true) {
+            alert('DetectRTC library is unable to find screen capturing support. You MUST run chrome with command line flag "chrome --enable-usermedia-screen-capturing"');
+          }
+      }
+      if (option !== 'Only Audio' && option !== 'Screen' && this.DetectRTC.hasWebcam !== true) {
+          alert('DetectRTC library is unable to find webcam; maybe you denied webcam access once and it is still denied or maybe webcam device is not attached to your system or another app is using same webcam.');
+      }
+
+      this.setState({
+        video: true
+      })
+
+      var mediaConfig = {
+        // video: this.broadcastervideo,
+        video:  {
+          // mediaSource: "screen", // whole screen sharing
+          mediaSource: "window", // choose a window to share
+          // mediaSource: "application", // choose a window to share
+          width: {max: '1920'},
+          height: {max: '1080'},
+          frameRate: {max: '10'}
+        
+        },
+        onsuccess: function(stream) {
+            this.config.attachStream = stream;
+            
+            // this.videosContainer.appendChild(htmlElement);
+            console.log(this.config.attachStream)
+            callback && callback();
+        },
+        onerror: function() {
+            if (option === 'Only Audio') alert('unable to get access to your microphone');
+            else if (option === 'Screen') {
+                // if (location.protocol === 'http:') alert('Please test this WebRTC experiment on HTTPS.');
+                alert('Screen capturing is either denied or not supported. Are you enabled flag: "Enable screen capture support in getUserMedia"?');
+            } else alert('unable to get access to your webcam');
+        }
+    };
+    if (constraints) mediaConfig.constraints = constraints;
+    // navigator.mediaDevices.getUserMedia(mediaConfig)
+    //   .then((stream) => {
+    //     this.onSuccessWithSrcObject(stream)
+    //   })
+    navigator.mediaDevices.getDisplayMedia({
+      video: true
+    }).then(screenStream => {
+      this.onSuccessWithSrcObject(screenStream)
+    })
+
+  }
+
+  onSuccessWithSrcObject = (stream) => {
+    this.broadcastervideo.srcObject = stream
   }
 
   render(){
@@ -84,7 +169,7 @@ class App extends React.Component {
     <div className="App">
       <section className="experiment">
         <section>
-          <select id="broadcasting-option">
+          <select id="broadcasting-option" name='option' value={this.state.option} onChange={this.handleChange}>
             <option value="Audio + Video">Audio + Video</option>
             <option value="Only Audio">Only Audio</option>
             <option value="Screen">Screen</option>
@@ -94,20 +179,23 @@ class App extends React.Component {
             id="broadcast-name"
             ref={this.broadcastname}
             name='broadcastName'
-            onChange={this.onChange}
+            onChange={this.handleChange}
             value={this.state.broadcastName}
             />
           <button 
             id="setup-new-broadcast" 
             className="setup"
             ref={this.setupnewbroadcast}
+            onClick={this.setupNewBroadcastButtonClickHandler}
             >Setup New Broadcast</button>
 
           <table id="rooms-list"></table>
           <div 
             id="videos-container"
             ref={this.videocontainer}
-            ></div>
+            >
+              {this.state.video ? <video id="peerVideo" ref={el => this.broadcastervideo = el} autoPlay/> : null}
+            </div>
         </section>
       </section>
     </div>
